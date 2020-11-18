cf25299b6d3d4ad5e6ae3a5e59aa5c3457bbc1d4
Sébastien <sebgeiser@gmail.com>
2020-11-18 20:13:33 +0100

    Add Basic Eval Markup

---
+++ CodingSeb.Mvvm/UIHelpers/CustomMarkups/Eval.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace CodingSeb.Mvvm.UIHelpers
+{
+    /// <summary>
+    /// Allow to execute small C# expressions in Xaml
+    /// </summary>
+    public class Eval : MarkupExtension
+    {
+        #region Constructor and ManageArgs
+
+        public Eval()
+        { }
+
+        public Eval(string expression)
+        {
+            Expression = expression;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// The Expression to Evaluate
+        /// </summary>
+        [ConstructorArgument("expression")]
+        public string Expression { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var evaluator = new ExpressionEvaluator.ExpressionEvaluator();
+
+            return evaluator.Evaluate(Expression);
+        }
+    }
+}
