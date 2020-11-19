89558805a6dfcd188735723da4ffc0bcad68bb70
Sébastien Geiser <sgeiser@precitrame.com>
2020-11-19 09:43:21 +0100

    More constructors for XMultiBinding + Refactoring

--- CodingSeb.Mvvm/UIHelpers/CustomMarkups/XMultiBinding.cs
+++ CodingSeb.Mvvm/UIHelpers/CustomMarkups/XMultiBinding.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Markup;
 
 namespace CodingSeb.Mvvm.UIHelpers
@@ -17,73 +18,34 @@ namespace CodingSeb.Mvvm.UIHelpers
         public XMultiBinding()
         { }
 
-        public XMultiBinding(MarkupExtension markup1)
-        {
-            Children.Add(markup1);
-        }
+        public XMultiBinding(MarkupExtension markup1) => Children.Add(markup1);
 
         public XMultiBinding(MarkupExtension markup1, MarkupExtension markup2)
-        {
-            Children.Add(markup1);
-            Children.Add(markup2);
-        }
+            : this(markup1) => Children.Add(markup2);
 
         public XMultiBinding(MarkupExtension markup1, MarkupExtension markup2, MarkupExtension markup3)
-        {
-            Children.Add(markup1);
-            Children.Add(markup2);
-            Children.Add(markup3);
-        }
+            : this(markup1, markup2) => Children.Add(markup3);
 
         public XMultiBinding(MarkupExtension markup1, MarkupExtension markup2, MarkupExtension markup3, MarkupExtension markup4)
-        {
-            Children.Add(markup1);
-            Children.Add(markup2);
-            Children.Add(markup3);
-            Children.Add(markup4);
-        }
+            : this(markup1, markup2, markup3) => Children.Add(markup4);
 
         public XMultiBinding(MarkupExtension markup1, MarkupExtension markup2, MarkupExtension markup3, MarkupExtension markup4, MarkupExtension markup5)
-        {
-            Children.Add(markup1);
-            Children.Add(markup2);
-            Children.Add(markup3);
-            Children.Add(markup4);
-            Children.Add(markup5);
-        }
+            : this(markup1, markup2, markup3, markup4) => Children.Add(markup5);
 
         public XMultiBinding(MarkupExtension markup1, MarkupExtension markup2, MarkupExtension markup3, MarkupExtension markup4, MarkupExtension markup5, MarkupExtension markup6)
-        {
-            Children.Add(markup1);
-            Children.Add(markup2);
-            Children.Add(markup3);
-            Children.Add(markup4);
-            Children.Add(markup5);
-            Children.Add(markup6);
-        }
+            : this(markup1, markup2, markup3, markup4, markup5) => Children.Add(markup6);
 
         public XMultiBinding(MarkupExtension markup1, MarkupExtension markup2, MarkupExtension markup3, MarkupExtension markup4, MarkupExtension markup5, MarkupExtension markup6, MarkupExtension markup7)
-        {
-            Children.Add(markup1);
-            Children.Add(markup2);
-            Children.Add(markup3);
-            Children.Add(markup4);
-            Children.Add(markup5);
-            Children.Add(markup6);
-            Children.Add(markup7);
-        }
+            : this(markup1, markup2, markup3, markup4, markup5, markup6) => Children.Add(markup7);
 
         public XMultiBinding(MarkupExtension markup1, MarkupExtension markup2, MarkupExtension markup3, MarkupExtension markup4, MarkupExtension markup5, MarkupExtension markup6, MarkupExtension markup7, MarkupExtension markup8)
-        {
-            Children.Add(markup1);
-            Children.Add(markup2);
-            Children.Add(markup3);
-            Children.Add(markup4);
-            Children.Add(markup5);
-            Children.Add(markup6);
-            Children.Add(markup7);
-            Children.Add(markup8);
-        }
+            : this(markup1, markup2, markup3, markup4, markup5, markup6, markup7) => Children.Add(markup8);
+
+        public XMultiBinding(MarkupExtension markup1, MarkupExtension markup2, MarkupExtension markup3, MarkupExtension markup4, MarkupExtension markup5, MarkupExtension markup6, MarkupExtension markup7, MarkupExtension markup8, MarkupExtension markup9)
+            : this(markup1, markup2, markup3, markup4, markup5, markup6, markup7, markup8) => Children.Add(markup9);
+        
+        public XMultiBinding(MarkupExtension markup1, MarkupExtension markup2, MarkupExtension markup3, MarkupExtension markup4, MarkupExtension markup5, MarkupExtension markup6, MarkupExtension markup7, MarkupExtension markup8, MarkupExtension markup9, MarkupExtension markup10)
+            : this(markup1, markup2, markup3, markup4, markup5, markup6, markup7, markup8, markup9) => Children.Add(markup10);
 
         #endregion
 
@@ -91,6 +53,20 @@ namespace CodingSeb.Mvvm.UIHelpers
 
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
+            return ProvideValue(serviceProvider, false);
+        }
+
+        public object ProvideValue(IServiceProvider serviceProvider, bool hierarchyBuilding)
+        {
+            if (!(serviceProvider.GetService(typeof(IProvideValueTarget)) is IProvideValueTarget service))
+                return this;
+
+            if (!(service.TargetObject is DependencyObject targetObject)
+                || !(service.TargetProperty is DependencyProperty targetProperty))
+            {
+                return this;
+            }
+
             return "Hye";
         }
     }
