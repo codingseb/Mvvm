0ad74735bd98145c4993c553af902539f411c60b
Sébastien Geiser <sgeiser@precitrame.com>
2021-08-12 08:22:28 +0200

    Update

---
+++ CodingSeb.Mvvm/UIHelpers/CustomMarkups/StringSource.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace WPFMvvm.UIHelpers
+{
+    /// <summary>
+    /// An Markup to easily provide a inline string as binding source
+    /// </summary>
+    public class StringSource : MarkupExtension
+    {
+        public StringSource()
+        {}
+
+        public StringSource(string stringValue)
+        {
+            StringValue = stringValue;
+        }
+
+        public string StringValue { get; set; } = string.Empty;
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return StringValue;
+        }
+    }
+}
